4faa14f3e2dd54efc57960348df30d8067c9f0d2
Juxsrck <73861576+Juxsrck@users.noreply.github.com>
2023-09-16 00:43:20 -0500

    Update EmpleadoService.java

--- src/main/java/services/EmpleadoService.java
+++ src/main/java/services/EmpleadoService.java
@@ -3,6 +3,8 @@ package services;
 import dao.EmpleadoDAO;
 import entities.Empleado;
 
+import java.util.List;
+
 public class EmpleadoService {
     private final EmpleadoDAO empleadoDAO;
 
@@ -12,5 +14,17 @@ public class EmpleadoService {
 
     public void crearEmpleado(Empleado empleado){
         empleadoDAO.saveEmpleado(empleado);
+    }
+
+    public List<Empleado> obtenerTodosLosEmpleados(){
+        return empleadoDAO.getAllEmpleados();
+    }
+
+    public void editarEmpleado(Empleado empleado){
+        empleadoDAO.updateEmpleado(empleado);
+    }
+
+    public void eliminarEmpleado(Long id){
+        empleadoDAO.deleteEmpleado(id);
     }
 }
