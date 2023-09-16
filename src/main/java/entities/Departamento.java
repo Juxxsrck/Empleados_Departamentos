935e7a1aaf9551fedc88bd7f5e86a84f1285ddc4
Juxsrck <73861576+Juxsrck@users.noreply.github.com>
2023-09-15 22:23:28 -0500

    Create Departamento.java

---
+++ src/main/java/entities/Departamento.java
@@ -0,0 +1,30 @@
+package entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "departamentos")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "departamento_codigo")
+    private String departamentoCodigo;
+
+    @Column(name = "departamento_nombre")
+    private String departamentoNombre;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_ora_crea")
+    private Date fechaHoraCrea;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_hora_modifica")
+    private Date fechaHoraModifica;
+}
